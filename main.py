1b2f07d24c5a17176a858acd56096dc174e7dc13
anoushk1234 <anoushka1828@gmail.com>
2022-06-01 13:40:18 +0530

    cmd

--- main.py
+++ main.py
@@ -18,14 +18,4 @@ async def on_ready():
     print(f"Logged in as {bot.user}")
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
-
 bot.run(os.environ["DISCORD_TOKEN"])
