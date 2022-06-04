3e42bb7d1a0dee1bf78b2385722f29f78633997e
anoushk1234 <anoushka1828@gmail.com>
2022-06-04 11:54:03 +0530

    finally works

--- main.py
+++ main.py
@@ -3,14 +3,33 @@
 import os
 import disnake
 from disnake.ext import commands
+import json
 
 
-bot = commands.InteractionBot()
+intent = disnake.Intents(message_content=True, messages=True, guilds=True)
+bot = commands.Bot(command_prefix="!", sync_commands=True, intents=intent)
 
 
-@bot.slash_command()
-async def ping(inter):
-    await inter.response.send_message("Pong!")
+@bot.slash_command(description="Responds with 'World'")
+async def hello(inter):
+    await inter.response.send_message("World")
+
+
+@bot.slash_command(name="hex", description="Gives anchor error for certain hex code")
+async def multiply(inter, hexcode: str):
+    print(hexcode, "hex")
+    f = open("codes.json")
+    data = json.loads(f.read())
+    # print(data)
+    error_obj = data[hexcode]
+    msg = error_obj["message"]
+    print(error_obj, msg)
+    await inter.response.send_message(msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
 
 
 @bot.event
