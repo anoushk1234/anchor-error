d2f44a3c120cc4eba326672af18395f0d3327b9e
anoushk1234 <anoushka1828@gmail.com>
2022-06-05 11:42:51 +0530

    added exception

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ intent = disnake.Intents(message_content=True, messages=True, guilds=True)
 bot = commands.Bot(command_prefix="!", sync_commands=True, intents=intent)
 
 
-@bot.slash_command(description="Responds with 'World'")
-async def hello(inter):
-    await inter.response.send_message("World")
+@bot.slash_command(name="health", description="health check")
+async def health(inter):
+    await inter.response.send_message("200 OK")
 
 
 @bot.slash_command(name="hex", description="Gives anchor error for certain hex code")
@@ -21,10 +21,17 @@ async def multiply(inter, hexcode: str):
     f = open("codes.json")
     data = json.loads(f.read())
     # print(data)
-    error_obj = data[hexcode]
-    msg = error_obj["message"]
-    print(error_obj, msg)
-    await inter.response.send_message(msg)
+    try:
+        error_obj = data[hexcode]
+        msg = error_obj["message"]
+        print(error_obj, msg)
+        await inter.response.send_message(msg)
+    except KeyError:
+        await inter.response.send_message(
+            "Invalid hex code or code doesnt exist in db, considering contributing at <https://github.com/anoushk1234/anchor-error>"
+        )
+    except:
+        await inter.response.send_message("an unknown error has occured")
 
 
 @bot.command()
